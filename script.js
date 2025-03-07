fb95e3c1be9b483763776e30a3264388c5da5a96
Cooper <cooperbecon@icloud.com>
2025-03-07 20:13:33 +1100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const facts = document.querySelectorAll('#rat-facts li');
-
-    facts.forEach(fact => {
-        fact.addEventListener('click', function() {
-            this.classList.toggle('highlight');
-        });
-    });
-});
