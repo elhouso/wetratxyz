2f9865fa62b5995c3a56e06da998505fac3c29cf
House <cooperbecon@icloud.com>
2025-03-21 12:56:03 +1100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".fade-in").forEach((el, index) => {
-        setTimeout(() => {
-            el.style.animationDelay = `${index * 0.3}s`;
-            el.classList.add("fade-in");
-        }, 100);
-    });
-});
